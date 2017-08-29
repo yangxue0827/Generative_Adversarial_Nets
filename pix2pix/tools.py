9772d6aafb16da7b4c00fde9957e5432db47e7c7
yangxue0827 <yangxue0827@126.com>
2017-08-29 09:18:54 +0800

    tools

---
+++ pix2pix/tools.py
@@ -0,0 +1,17 @@
+import math
+import sys
+import os
+
+
+def view_bar(message, num, total):
+    rate = num / total
+    rate_num = int(rate * 40)
+    rate_nums = math.ceil(rate * 100)
+    r = '\r%s:[%s%s]%d%%\t%d/%d' % (message, ">" * rate_num, " " * (40 - rate_num), rate_nums, num, total,)
+    sys.stdout.write(r)
+    sys.stdout.flush()
+
+
+def mkdir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
